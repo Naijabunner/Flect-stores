40d67db8fdad37ac251beb34c64c807ce406d177
Naiabunner <Okechukwudaniel12@gmail.com>
2023-11-01 04:16:47 +0000

    Added empty cart slide and styling

--- flect/src/empty_cart.jsx
+++ flect/src/empty_cart.jsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 const Emptycart = () => {
+    const navigate =useNavigate()
+
+    const handlesubmit_csbtn=()=>{
+        navigate("/")
+    }
     return ( <>
+    <div className="emptycart_cont">
     <header className="emptycart_txt">
         Your cart is empty
     </header>
+    <img src="src\assets\person-shopping.jpg" alt="Empty cart image" />
+        <button className="countinue_shoppingbtn" onClick={handlesubmit_csbtn}>Contiue Shopping</button>
+    </div>
+    
     </> );
 }
  
